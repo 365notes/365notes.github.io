049ec360694eb9b575a81be3fc169bea1b52552f
joeyShea <shea.number.2@gmail.com>
2024-12-22 13:17:27 -0600

    Scrolling fix?

--- startup.js
+++ startup.js
@@ -76,5 +76,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("popup").style.display = "block";
     document.getElementById("black").style.display = "block";
     document.body.classList.add("no-scroll");
+
+    function preventScrolling(e) {
+      e.preventDefault();
+    }
+    document.body.addEventListener("touchmove", preventScrolling, {
+      passive: false,
+    });
   }
 });
